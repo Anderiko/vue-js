0398a06c59b9bc0f3de81c97d54900f877977ca9
MAISDUSE <mathieu.duse@hotmail.fr>
2021-02-10 17:13:24 +0100

    affichage dans index

--- src/main.js
+++ src/main.js
@@ -6,4 +6,20 @@ var app = new Vue({
 })
 
 
-console.log(sortTable);
+
+// Our data object
+let dataFirstObject = sortTable[0];
+let cate = ["Livre","Nom","Ecole", "Branches", "Classes", "Composantes", "Temps d:\'incantation", "Portée", "Cible", "Durée", "Jet de sauvegarde", "Résistance à la magie", "Description", "Nécessaire"];
+
+// The object is added to a Vue instance
+var vm = new Vue({
+  el: '#vm',
+  data: {
+    obj: dataFirstObject,
+    cate: cate
+  }
+})
+
+
+
+console.log(sortTable[0]);
